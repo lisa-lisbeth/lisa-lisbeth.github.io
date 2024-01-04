cc573e643009cad515124c8de387588dd0e15efa
lisa-lisbeth <73475484+lisa-lisbeth@users.noreply.github.com>
2024-01-04 11:26:31 +0100

    data.js

--- data.js
+++ data.js
@@ -1,84 +1,47 @@
 const data = [
   {
-    itemName: "Nanotech Fabrics",
-    itemCategory: "Eco-Futurism Line",
-    itemLink: "https://www.youtube.com/@codegrid",
-    itemCopy:
-      "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, it's the dawn of apparel that truly works for you.",
+    itemName: "Johanna Klug",
+    itemCategory: "Webdesign",
+    itemLink: "https://johanna-klug.de/",
+    itemCopy: "Website Gestaltung und Umsetzung mit WordPress und Oxygen Page Builder für die Autorin und Sterbe- und Trauerbegleiterin Johanna Klug.",
     itemImg: "./img-1.jpg",
   },
   {
-    itemName: "Bio-luminescent Accents",
-    itemCategory: "Galactic Glam",
-    itemLink: "https://codegrid.gumroad.com/l/codegridpro",
-    itemCopy:
-      "Nature and fashion converge with Natural Bio-luminescent Accents. By harnessing the power of organisms that glow, designers are creating ethereal fashion statements that light up any setting. These luminescent highlights shimmer with an otherworldly charm, transforming the wearer into a radiant spectacle, be it at a beach party under the stars or a glamorous nighttime event.",
+    itemName: "Brasse Trinkvergnügen",
+    itemCategory: "Grafikdesign",
+    itemLink: "https://assets.adobe.com/id/urn:aaid:sc:US:44229ee7-07c0-46aa-963a-5a5c28897387?view=published",
+    itemCopy: "Design von der Getränkekarte für die Bar Brasse.",
     itemImg: "./img-2.jpg",
   },
   {
-    itemName: "Integrated Wearables",
-    itemCategory: "Neural Nuance",
-    itemLink: "https://twitter.com/codegridweb",
-    itemCopy:
-      "Fashion takes a digital leap with Integrated Electronic Wearables. It's not just about wearing technology—it's about integrating it seamlessly into everyday style. From garments that light up with embedded LEDs to outfits that can display your current mood through colors and patterns, the future of fashion is connected, smart, and always evolving.",
+    itemName: "Rabbit Radio",
+    itemCategory: "Webdesign",
+    itemLink: "https://rabbitradio.de/",
+    itemCopy: "Konzept und Realisierung des Webauftritt-Relaunchs für das Campusradio Rabbit Radio im Rahmen meiner Bachelorarbeit.",
     itemImg: "./img-3.jpg",
   },
   {
-    itemName: "Morphing Clothing",
-    itemCategory: "Eco-Futurism Line",
-    itemLink: "https://www.youtube.com/@codegrid",
-    itemCopy:
-      "Imagine apparel that evolves with your mood or the environment. Dynamic Morphing Clothing promises exactly that. With the ability to change hue, design, or even its texture, this adaptive fashion ensures you have a fresh look whenever you desire. Be it a change in the weather or a change in your mood, your attire adapts effortlessly.",
+    itemName: "Interactive Design",
+    itemCategory: "UI/UX",
+    itemLink: "https://xd.adobe.com/view/72a3965e-694a-4708-865b-8a63b8a6f71d-27f0/screen/f0852cc4-a9f8-4165-becf-753f35930306/",
+    itemCopy: "Umsetzung eines Prototypen mit Axure und eines Clickdummys mit XD einer eLearning-App im Rahmen einer Studienarbeit.",
     itemImg: "./img-4.jpg",
   },
   {
-    itemName: "3D Holographic Prints",
-    itemCategory: "Digital Elegance",
-    itemLink: "https://twitter.com/codegridweb",
-    itemCopy:
-      "Elevate your style quotient with 3D Holographic Prints. Moving away from the traditional two-dimensional patterns, these designs seem to hover off the fabric, creating an optical allure. As you move, the holographic patterns shift, ensuring that you're not only seen but remembered.",
+    itemName: "E-Publikation",
+    itemCategory: "Grafikdesign",
+    itemLink: "https://indd.adobe.com/view/fed56b75-3a64-4d06-81b5-82e799640dff",
+    itemCopy: "Erstellung einer interaktiven E-Publikation mit InDesign im Rahmen einer Studienarbeit.",
     itemImg: "./img-5.jpg",
   },
   {
-    itemName: "Atmospheric Garments",
-    itemCategory: "Eco-Futurism Line",
-    itemLink: "https://codegrid.gumroad.com/l/codegridpro",
-    itemCopy:
-      "For the adventurers who seek the skies or even other planets, Atmospheric Aero-adaptable Garments are a godsend. These outfits are crafted to adjust to varying atmospheric conditions. Whether you're exploring the high-altitude mountains or dreaming of interstellar travels, these garments promise comfort paired with avant-garde style.",
+    itemName: "Flamingo und Dosenbier",
+    itemCategory: "Webdesign",
+    itemLink: "https://flamingo-und-dosenbier.de/",
+    itemCopy: "Website Gestaltung und Umsetzung eines Online Magazins im Rahmen einer Gruppen-Studienarbeit. Meine Aufgabe war die Erstellung und Implementierung eines eigenen Wordpress-Themes mit HTML, CSS und PHP.",
     itemImg: "./img-1.jpg",
   },
-  {
-    itemName: "Responsive Skinsync",
-    itemCategory: "Neural Nuance",
-    itemLink: "https://twitter.com/codegridweb",
-    itemCopy:
-      "In a union of technology and biology, the Responsive Skinsync Attire emerges. This clothing intuitively interacts with the wearer's physiological signals. A rise in body temperature or even a blush could cause the fabric to change its color. It's not just wearing an outfit; it's forming a bond with it.",
-    itemImg: "./img-2.jpg",
-  },
-  {
-    itemName: "Sustainable Zero Weavings",
-    itemCategory: "Galactic Glam",
-    itemLink: "https://www.youtube.com/@codegrid",
-    itemCopy:
-      "As the fashion industry grapples with sustainability, Sustainable Zero-waste Weavings offer a beacon of hope. With meticulous design techniques, every thread, every weave has a purpose, ensuring that there's no waste and no environmental footprint. Fashion-forward and eco-conscious, it’s the way forward.",
-    itemImg: "./img-3.jpg",
-  },
-  {
-    itemName: "Lab Synthetic Leather",
-    itemCategory: "Eco-Futurism Line",
-    itemLink: "https://codegrid.gumroad.com/l/codegridpro",
-    itemCopy:
-      "Ethical and luxurious, Lab-crafted Synthetic Leather is redefining luxury. Created meticulously in controlled environments, it replicates the feel and aesthetics of traditional leather without the ethical concerns. It's a nod to the classics while being firmly rooted in the future.",
-    itemImg: "./img-4.jpg",
-  },
-  {
-    itemName: "Neuro Fashion Gear",
-    itemCategory: "Neural Nuance",
-    itemLink: "https://www.youtube.com/@codegrid",
-    itemCopy:
-      "The frontier of fashion meets neuroscience with Neuro-responsive Fashion Gear. These outfits are designed to understand and respond to neural signals. A simple thought can alter its appearance or function. It's not just an outfit; it's an extension of one's thoughts and emotions.",
-    itemImg: "./img-5.jpg",
-  },
+  
 ];
 
 export { data };
